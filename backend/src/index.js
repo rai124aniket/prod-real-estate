c2fbf3209cfa4937d23d7d468bb7bbe9c3424453
rai124aniket <raianiket124@gmail.com>
2025-11-23 21:34:45 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://prod-real-estate-frontend.onrender.com",
     credentials: true,
   })
 );
